e0872259b278348e10778a941cb008042df3893c
root <root@ip-172-31-15-48.us-east-2.compute.internal>
2020-09-18 06:41:37 +0000

    compiled HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello World! Jenkins build isSuccessful");
+    System.out.println("Hello World! Jenkins build is Successful");
   }
 }
